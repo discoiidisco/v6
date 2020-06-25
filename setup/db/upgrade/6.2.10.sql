747fd0de409db59e60beb1178022b96d15c48687
discoiidisco <59869890+discoiidisco@users.noreply.github.com>
2020-06-25 19:39:53 +0100

    Update 6.2.10.sql
    Add `printed` column. Enables viewing just unprinted orders in admin.

--- setup/db/upgrade/6.2.10.sql
+++ setup/db/upgrade/6.2.10.sql
@@ -1,5 +1,7 @@
 DELETE FROM `CubeCart_cookie_consent` WHERE `log_hash` = 'd41d8cd98f00b204e9800998ecf8427e'; #EOQ
 ALTER TABLE `CubeCart_coupons` ADD `coupon_per_customer` INT(10) UNSIGNED NULL DEFAULT NULL; #EOQ
+ALTER TABLE `CubeCart_order_summary` ADD `printed` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0'; #EOQ
+ALTER TABLE `CubeCart_order_summary` ADD KEY (`printed`); #EOQ
 CREATE TABLE `CubeCart_customer_coupon` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `customer_id` int(10) UNSIGNED NOT NULL,
